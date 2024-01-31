684ded95d24905ea2a4b5496b3bb8104ad0f505e
RafikM-Solicy <rafik.muradyan@solicy.net>
2024-01-31 11:35:20 +0400

    added rate limit and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+
 
 async function bootstrap(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -8,6 +12,13 @@ async function bootstrap(): Promise<NestExpressApplication> {
   });
 
   app.setGlobalPrefix('api');
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // requests limit
+    }),
+  );
 
   const port = process.env.PORT || 4000;
   await app.listen(port);
